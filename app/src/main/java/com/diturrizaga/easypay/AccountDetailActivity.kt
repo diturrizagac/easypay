6ae0a2adf3a671bce107c7afaf6a26cdfd615a6f
Diego Iturrizaga <diturrizagac@gmail.com>
2019-05-15 21:14:12 -0500

    first commit

---
+++ app/src/main/java/com/diturrizaga/easypay/AccountDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.diturrizaga.easypay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_detail)
+    }
+}
